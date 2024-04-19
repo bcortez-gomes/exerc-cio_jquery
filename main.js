755ededbdba4d9fdf708bb5d4fbf4dfc10980603
bcortez-gomes <160198522+bcortez-gomes@users.noreply.github.com>
2024-04-19 00:07:15 -0300

    adiconando arquivos ao repositório

---
+++ main.js
@@ -0,0 +1,47 @@
+const form = document.getElementById('form-tarefas');
+const tarefaSemana = [];
+
+let linhas = '';
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    adicionaTarefaSemana();
+    atualizaLista();
+})
+
+function adicionaTarefaSemana() {
+    const inputTarefaSemana = document.getElementById('nova-tarefa-adicionada');
+
+    if (tarefaSemana.includes(inputTarefaSemana.value)) {
+        alert(`A tarefa: ${inputTarefaSemana.value}, já está disponível na sua lista.`);
+    } else {
+        tarefaSemana.push(inputTarefaSemana.value);
+
+        let linha = '<li>';
+        linha += `${inputTarefaSemana.value}`;
+        linha += '</li>';
+
+        linhas += linha;
+}
+
+const listaDeTarefas = document.getElementById('nova-info');
+listaDeTarefas.innerHTML = linhas;
+
+inputTarefaSemana.value = '';
+}
+
+function atualizaLista() {
+    const listaDeTarefas = document.getElementById('nova-info');
+    listaDeTarefas.innerHTML - linhas;
+}
+
+const checkNaLista = document.querySelector('ul');
+
+checkNaLista.addEventListener('click', function(e){
+    if(e.target.classList.contains('checked')){
+        e.target.classList.remove('Checked');
+    } else {
+        e.target.classList.add('checked');
+    }
+})
